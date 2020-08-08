ee711841f1fd1705d21bf7e04d151ccdf7741f85
Lee Alexis <john20xdoe@users.noreply.github.com>
2020-08-08 15:02:14 +0800

    Minor cleanup on vector-web-setup logo (added neurral  systems)

--- vector-web-setup.js
+++ vector-web-setup.js
@@ -2,40 +2,44 @@
 
 /* Copyright (c) 2019-2020 Digital Dream Labs. See LICENSE file for details. */
 
-var commander = require("commander");
+var commander = require('commander');
 
 const program = new commander.Command();
 
 program
-  .version("1.0.0", "-v, --version")
+  .version('1.0.0', '-v, --version')
   .description(
-    " __      ________ _____ _______ ____  _____  \n" +
-      " \\ \\    / /  ____/ ____|__   __/ __ \\|  __ \\ \n" +
-      "  \\ \\  / /| |__ | |       | | | |  | | |__) |\n" +
-      "   \\ \\/ / |  __|| |       | | | |  | |  _  / \n" +
-      "    \\  /  | |___| |____   | | | |__| | | \\ \\ \n" +
-      "     \\/   |______\\_____|  |_|  \\____/|_|  \\_\\ \n\n"
+    `
+    __      ________  ____ ______ ____  _____   
+    \\ \\    / /  ___| / ___|__  __/ __ \\|  __ \\ 
+     \\ \\  / /| |__  | |     | | | |  | | |__) | 
+      \\ \\/ / |  __| | |     | | | |  | |  _  /  
+       \\  /  | |___ | |___  | | | |__| | | \\ \\  
+        \\/   |_____| \\____| |_|  \\____/|_|  \\_\\ 
+         N E U R R A L           S Y S T E M S 
+
+      `
   )
-  .addCommand(require("./tools/configure.js"))
-  .addCommand(require("./tools/ota.js").otaAdd)
-  .addCommand(require("./tools/ota.js").otaSync)
-  .addCommand(require("./tools/ota.js").otaApprove)
+  .addCommand(require('./tools/configure.js'))
+  .addCommand(require('./tools/ota.js').otaAdd)
+  .addCommand(require('./tools/ota.js').otaSync)
+  .addCommand(require('./tools/ota.js').otaApprove)
   .exitOverride(() => {
     process.exit(0);
   });
 
 program
-  .command("serve")
-  .description("Serve the vector websetup")
-  .option("-p, --port <number>", "port to serve the setup on", 8000)
+  .command('serve')
+  .description('Serve the vector websetup')
+  .option('-p, --port <number>', 'port to serve the setup on', 8000)
   .option(
-    "-ip, --ip-address <addr>",
-    "address to serve the setup on",
-    "0.0.0.0"
+    '-ip, --ip-address <addr>',
+    'address to serve the setup on',
+    '0.0.0.0'
   )
   .action((options) => {
     try {
-      require("./tools/run.js")(options.port, options.ipAddress);
+      require('./tools/run.js')(options.port, options.ipAddress);
     } catch (err) {
       console.log(err);
     }
